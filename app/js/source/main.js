bb042138a7a8f58066914f1069b6240678ba7b9a
CarterNelms <carternelms@bellsouth.net>
2014-04-30 10:25:26 -0500

    first working game

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -1,82 +1,81 @@
 (function()
 {
-  'use strict';
+	'use strict';
 
-  /* 0: Minimum row count
-   * 0: Mamimum row count
-   * 0: Minimum column count
-   * 0: Mamimum column count
-   */
-  var rowAndColCountRange = [3, 12, 3,12];
+	/* 0: Minimum row count
+	 * 0: Mamimum row count
+	 * 0: Minimum column count
+	 * 0: Mamimum column count
+	 */
+	var rowAndColCountRange = [3, 12, 3,12];
 
-  var $start;
-  var $board;
+	var $start;
+	var $board;
+	var chompSound = new Audio('media/sounds/pacman_chomp-new.wav');
 
-  $(document).ready(initialize);
+	$(document).ready(initialize);
 
-  function initialize()
-  {
+	function initialize()
+	{
 		$start = $('#start');
 		$board = $('#board');
 		$(window).resize(sizeSprites);
 		$start.click(start);
-  }
+	}
 
-  function start()
-  {
+	function start()
+	{
 		$start.off('click');
 		$('#home').css('visibility', 'hidden');
 		$('#menu').css('visibility', 'hidden');
 		$board.css('visibility', 'visible');
 
 		newRound();
-  }
+	}
 
-  function newRound()
-  {
+	function newRound()
+	{
 		setPlayerCanMove(true);
-  	buildGameBoard();
+		buildGameBoard();
 		setSprites();
 		checkForWin();
-  }
+	}
 
-  function buildGameBoard()
-  {
+	function buildGameBoard()
+	{
 		// The number of rows and columns
 		var rowAndColCount = [];
 		// The game cells' height and width
 		var cellDimensions = [];
 		for(var i = 0; i < 2; ++i)
 		{
-		  rowAndColCount[i] = Math.floor(Math.random()*(rowAndColCountRange[2*i+1]-rowAndColCountRange[2*i])+rowAndColCountRange[2*i]);
-		  cellDimensions[i] = (100/rowAndColCount[i])+'%';
+			rowAndColCount[i] = Math.floor(Math.random()*(rowAndColCountRange[2*i+1]-rowAndColCountRange[2*i])+rowAndColCountRange[2*i]);
+			cellDimensions[i] = (100/rowAndColCount[i])+'%';
 		}
 
 		for(var r = 0; r < rowAndColCount[0]; ++r)
 		{
-		  var $row = $('<div>').addClass('gameRow');
-		  $row.css('height', cellDimensions[0]);
+			var $row = $('<div>').addClass('gameRow');
+			$row.css('height', cellDimensions[0]);
 
-		  for(var c = 0; c < rowAndColCount[1]; ++c)
-		  {
+			for(var c = 0; c < rowAndColCount[1]; ++c)
+			{
 			var $cell = $('<div>').addClass('gameCell');
 			$cell.css('width', cellDimensions[1]);
 
-			// $cell.data('x', c);
-			// $cell.data('y', r);
 			$cell.attr({
-			  'data-x': c,
-			  'data-y': r
+				'data-x': c,
+				'data-y': r
 			});
 
 			$row.append($cell);
-		  }
-		  $board.append($row);
+			}
+			$board.append($row);
 		}
-  }
+	}
 
-  function setSprites()
-  {
+	function setSprites()
+	{
 		var $player = $('<div>').addClass('player');
 		var $pellet = $('<div>').addClass('pellet');
 
@@ -84,17 +83,17 @@
 		$randomCell().append($pellet);
 
 		sizeSprites();
-  }
+	}
 
-  function $randomCell()
-  {
+	function $randomCell()
+	{
 		var x = Math.floor(Math.random() * $('.gameRow:first-child .gameCell').length);
 		var y = Math.floor(Math.random() * $('.gameRow').length);
 		return $('.gameCell[data-x='+x+'][data-y='+y+']');
-  }
+	}
 
-  function sizeSprites()
-  {
+	function sizeSprites()
+	{
 		var $anyOldCell = $('.gameCell[data-x=0][data-y=0]');
 		var cellHeight = $anyOldCell.height();
 		var cellWidth = $anyOldCell.width();
@@ -121,32 +120,36 @@
 				left: (cellWidth-thisLength)/2
 			});
 		}
-  }
+	}
 
-  function move(event)
-  {
-  	var key = event.keyCode;
-  	if(key >= 37 && key <= 40)
+	function movePlayer(event)
+	{
+		var key = event.keyCode;
+		if(key >= 37 && key <= 40)
 		{
 			event.preventDefault();
 
 			var $oldCell = getCell($('.player'));
 			var xOffset = 0;
 			var yOffset = 0;
-
+			var rotationAngle;
 			switch(event.keyCode)
 			{
 				case 38:
 					yOffset -= 1;
+					rotationAngle = 270;
 					break;
 				case 40:
 					yOffset += 1;
+					rotationAngle = 90;
 					break;
 				case 37:
 					xOffset -= 1;
+					rotationAngle = 180;
 					break;
 				case 39:
 					xOffset += 1;
+					rotationAngle = 0;
 			}
 
 			var oldPos = getPos($oldCell);
@@ -157,145 +160,254 @@
 
 			if($newCell.length)
 			{
-				$newCell.append($('.player'));
+				var $player = $('.player');
+				var $animator = $('<div>').addClass('animator');
+
+				$oldCell.append($animator);
+				$animator.append($player);
+
+				var leftOffset = $newCell.offset().left - $oldCell.offset().left;
+				var topOffset = $newCell.offset().top - $oldCell.offset().top;
+
+				rotatePlayer(rotationAngle);
+
+				var moveTime = 450;
+
+				triggerEatAnimation(moveTime, 4);
+
+				loopAudio(chompSound, moveTime);
+
+				$animator.animate(
+					{
+						left: leftOffset,
+						top: topOffset
+					},
+					moveTime,
+					'linear',
+					function()
+					{
+						$newCell.append($player);
+						$animator.remove();
+
+						checkForWin();
+					}	
+				);
 			}
-
-			checkForWin();
 		}
-  }
+	}
 
-  function checkForWin()
-  {
-  	var $player = $('.player');
-  	var $pellet = $('.pellet');
+	// function loopAudio(audio, time)
+	// {
+	// 	audio.play();
+	// 	setTimeout(function()
+	// 	{
+	// 		audio.stop();
+	// 	},
+	// 	time
+	// 	);
+	// }
 
-  	var $playerCell = getCell($player);
-  	var $pelletCell = getCell($pellet);
+	function loopAudio(audio, playTime)
+	{
+		var startTime = Date.now();
+		audio.addEventListener('ended', onEnded, false);
+		audio.play();
+		loop();
 
-  	var playerPos = getPos($playerCell);
-  	var pelletPos = getPos($pelletCell);
+		function loop()
+		{
+			var currentTime = Date.now();
+			var timePassed = currentTime - startTime;
+			//console.log(timePassed);
+			if(timePassed >= playTime)
+			{
+				//audio.load();
+				//audio.play();
+			// }
+			// else
+			// {
+				console.log('Should end');
+				audio.removeEventListener('ended', onEnded, false);
+			}
+		}
 
-  	if(playerPos[0] === pelletPos[0] && playerPos[1] === pelletPos[1])
-  	{
-  		openMenu('You win!');
-  	}
-  }
+		function onEnded()
+		{
+			this.play();
+			loop();
+		}
+	}
 
-  function openMenu(text)
-  {
-  	setPlayerCanMove(false);
-  	$('#menu').css('visibility', 'visible');
+	function triggerEatAnimation(time, bites)
+	{
+		var bitesPerSecond = bites/(time/1000);
+		var deltaTime = 1000/bitesPerSecond;
 
-  	var options = [
-  		'Continue',
-  		'Quit'
-  	];
+		var $player = $('.player');
+		var counter = 0;
+		eat();
+		var mouthAnim = setInterval(function()
+		{
+			if(++counter < bites)
+			{
+				eat();
+			}
+			else
+			{
+				clearInterval(mouthAnim);
+				$player.removeClass('animFrame2');
+			}
+		},
+		deltaTime
+		);
 
-  	var $menuOptions = $('#menuOptions');
+		function eat()
+		{
+			$player.toggleClass('animFrame2');
+		}
+	}
 
-  	$menuOptions.children().remove();
+	function checkForWin()
+	{
+		var $player = $('.player');
+		var $pellet = $('.pellet');
 
-  	for(var i = 0; i < options.length; ++i)
-  	{
-  		var $thisOption = $('<div>');
-  		$thisOption.text(options[i]);
-  		$menuOptions.append($thisOption);
-  	}
+		var $playerCell = getCell($player);
+		var $pelletCell = getCell($pellet);
 
-  	$('#menuMessage').text(text);
+		var playerPos = getPos($playerCell);
+		var pelletPos = getPos($pelletCell);
 
-  	$('body').keydown(selectOptionKey);
-  	$menuOptions.on('click', 'div', selectOptionMouse);
-  }
+		if(playerPos[0] === pelletPos[0] && playerPos[1] === pelletPos[1])
+		{
+			openMenu('You win!');
+		}
+	}
 
-  function selectOptionKey(event)
-  {
-  	var key = event.keyCode;
+	function openMenu(text)
+	{
+		setPlayerCanMove(false);
+		$('#menu').css('visibility', 'visible');
 
-  	var option;
+		var options = [
+			'Continue',
+			'Quit'
+		];
 
-  	if(key === 67 || key === 81)
-  	{
-  		switch(key)
-  		{
-  			case 67:
-  				option = 'continue';
-  				break;
-  			default:
-  				option = 'quit';
-  		}
-  		selectOption(option);
-  	}
-  }
+		var $menuOptions = $('#menuOptions');
 
-  function selectOptionMouse()
-  {
-  	selectOption($(this).text());
-  }
+		$menuOptions.children().remove();
 
-  function selectOption(option)
-  {
-  	endRound();
+		for(var i = 0; i < options.length; ++i)
+		{
+			var $thisOption = $('<div>');
+			$thisOption.text(options[i]);
+			$menuOptions.append($thisOption);
+		}
 
-  	stopMenuListen();
+		$('#menuMessage').text(text);
 
-  	switch(option.toLowerCase())
-  	{
-  		case 'continue':
-  			newRound();
-  			break;
-  		case 'quit':
-  			quit();
-  			break;
-  		default:
-  	}
-  }
+		$('body').keydown(selectOptionKey);
+		$menuOptions.on('click', 'div', selectOptionMouse);
+	}
 
-  function endRound()
-  {
+	function selectOptionKey(event)
+	{
+		var key = event.keyCode;
+
+		var option;
+
+		if(key === 67 || key === 81)
+		{
+			switch(key)
+			{
+				case 67:
+					option = 'continue';
+					break;
+				default:
+					option = 'quit';
+			}
+			selectOption(option);
+		}
+	}
+
+	function selectOptionMouse()
+	{
+		selectOption($(this).text());
+	}
+
+	function selectOption(option)
+	{
+		endRound();
+
+		stopMenuListen();
+
+		switch(option.toLowerCase())
+		{
+			case 'continue':
+				newRound();
+				break;
+			case 'quit':
+				quit();
+				break;
+			default:
+		}
+	}
+
+	function endRound()
+	{
 		$('.player').remove();
 		$('.pellet').remove();
 		$('#board > *').remove();
-  }
+	}
 
-  function quit()
-  {
+	function quit()
+	{
 		$('#menu').css('visibility', 'hidden');
 		$board.css('visibility', 'hidden');
 		$('#home').css('visibility', 'visible');
 		$start.click(start);
-  }
+	}
 
-  function stopMenuListen()
-  {
-  	$('#menu').off('click');
-  	$('body').off('keydown');
-  	$('#menu').css('visibility', 'hidden');
-  }
+	function stopMenuListen()
+	{
+		$('#menu').off('click');
+		$('body').off('keydown');
+		$('#menu').css('visibility', 'hidden');
+	}
 
-  function setPlayerCanMove(isPlayerMobile)
-  {
-  	if(isPlayerMobile)
-  	{
-  		$('body').keydown(move);
-  	}
-  	else
-  	{
-  		$('body').off('keydown');  	
-  	}
-  }
+	function setPlayerCanMove(isPlayerMobile)
+	{
+		if(isPlayerMobile)
+		{
+			$('body').keydown(movePlayer);
+		}
+		else
+		{
+			$('body').off('keydown');  	
+		}
+	}
 
-  function getPos($cell)
-  {
-  	return [
-  		$cell.data('x'),
-  		$cell.data('y')
-  	];
-  }
+	function getPos($cell)
+	{
+		return [
+			$cell.data('x'),
+			$cell.data('y')
+		];
+	}
 
-  function getCell($child)
-  {
-  	return $child.parent('.gameCell');
-  }
+	function getCell($child)
+	{
+		return $child.parent('.gameCell');
+	}
+
+	function rotatePlayer(angle)
+	{
+		$('.player').css({
+			'-webkit-transform' : 'rotate('+ angle +'deg)',
+			'-moz-transform' : 'rotate('+ angle +'deg)',
+			'-ms-transform' : 'rotate('+ angle +'deg)',
+			'transform' : 'rotate('+ angle +'deg)'});
+	}
 
 })();
